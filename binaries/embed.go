8d6dc62e5074a5e5de2ad59867884f4f6a95626f
Chat Vo <chat.vo@cubicstack.net>
2025-11-17 15:47:38 +0700

    ttttt

--- binaries/embed.go
+++
@@ -1,5 +0,0 @@
-package binaries
-
-// Note: Binaries are now downloaded at runtime from GitHub releases
-// instead of being embedded in the application.
-// See downloader.go for the download implementation.
--- binaries/linux/embed_linux.go
+++
@@ -1,6 +0,0 @@
-//go:build linux
-
-package binaries
-
-// Note: Binaries are now downloaded at runtime from GitHub releases.
-// This file is kept for compatibility but no longer embeds binaries.
